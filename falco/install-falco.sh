55c4bad5e38d058409e4357507ae400264cba58b
laborant <laborant@labs.iximiuz.com>
2026-03-29 06:48:14 +0000

    Add Kubernetes security lab files and Falco setup

---
+++ falco/install-falco.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+helm repo add falcosecurity https://falcosecurity.github.io/charts
+helm repo update
+helm install falco falcosecurity/falco \
+  --namespace falco \
+  --create-namespace \
+  --set driver.kind=modern_ebpf
